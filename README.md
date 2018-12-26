56f36e7aff5c6b1ad459db4a55e17f65f3273248
wuyuhang <wuyuhang@zhphfinance.com>
2018-12-26 11:15:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+## VUE-SSR
+
+
+#### Usage:
+
+```shell
+yarn 
+
+yarn dev # 开发环境
+
+yarn build # 线上构建
+yarn start # 线上运行
+```
+
---
+++ build/webpack.server.conf.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const nodeExternals = require('webpack-node-externals')
+const baseWebpackConfig = require('./webpack.base.conf')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+const resolve = dir => path.resolve(__dirname,"..", dir)
+
+module.exports = merge(baseWebpackConfig, {
+  mode: 'production',
+  target: 'node',
+  devtool: 'source-map',
+  entry:resolve('src/entry-server.js'),
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'server-bundle.js',
+  },
+  externals: nodeExternals({
+    whitelist: /\.css$/
+  }),
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.VUE_ENV': '"server"'
+    }),
+    // 默认文件名为 `vue-ssr-server-bundle.json`
+    new VueServerPlugin()
+  ]
+})
